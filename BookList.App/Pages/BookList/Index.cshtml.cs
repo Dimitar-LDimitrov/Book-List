ea3435ace34fd9fc6bf3dba261a5920cc2d6e415
Dimitrov <dimitrar-ldimitrovv@gmail.com>
2021-04-05 13:34:17 +0300

    Implementing Delete Button

--- BookList.App/Pages/BookList/Index.cshtml.cs
+++ BookList.App/Pages/BookList/Index.cshtml.cs
@@ -7,7 +7,8 @@ namespace BookList.App.Pages.BookList
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using System.Linq;
-    
+    using Microsoft.AspNetCore.Mvc;
+
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext context;
@@ -24,9 +25,17 @@ namespace BookList.App.Pages.BookList
             this.Books = await this.context.Book.ToListAsync();
         }
 
-        //public IEnumerable<Book> GetAllBooks()
-        //{
-        //    return this.context.
-        //}
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await this.context.Book.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            this.context.Remove(book);
+            await this.context.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
     }
 }
