9b58503ef277523a0433a967c1b1df149936ce32
Dimitrov <dimitrar-ldimitrovv@gmail.com>
2021-04-05 12:52:55 +0300

    Add Validations for Create Form

--- BookList.App/Pages/BookList/Create.cshtml.cs
+++ BookList.App/Pages/BookList/Create.cshtml.cs
@@ -2,7 +2,9 @@ namespace BookList.App.Pages.BookList
 {
     using global::BookList.App.Data;
     using global::BookList.App.Models;
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
+    using System.Threading.Tasks;
 
     public class CreateModel : PageModel
     {
@@ -12,11 +14,25 @@ namespace BookList.App.Pages.BookList
         {
             this.context = context;
         }
-
+        [BindProperty]
         public Book Book { get; set; }
 
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await this.context.Book.AddAsync(Book);
+                await this.context.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
